0bca803e5ddf99c80790b832e0ff784dc33e8913
Sebastian <sebastian@nielsen2u.dk>
2020-04-17 00:16:45 +0200

    Added default connection string

--- VareDatabase/VareDatabase/Startup.cs
+++ VareDatabase/VareDatabase/Startup.cs
@@ -25,9 +25,11 @@ namespace VareDatabase
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<SomeDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllers();
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
