30fd6cfc35fc286fdf8ceea143e6824076660775
Sebastian <sebastian@nielsen2u.dk>
2020-05-01 17:01:17 +0200

    unit of work log in

--- VareDatabase/VareDatabase/Repo/LogInUnitOfWork.cs
+++ VareDatabase/VareDatabase/Repo/LogInUnitOfWork.cs
@@ -2,10 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using VareDatabase.Interfaces;
+using VareDatabase.DBContext;
+using Microsoft.EntityFrameworkCore;
 
 namespace VareDatabase.Repo
 {
-    public class LogInUnitOfWork
+    public class LogInUnitOfWork : IUnitOfWork
     {
+        public DbContext Context { get; protected set; }
+        public LogInUnitOfWork(/*LogInContext context*/)
+        {
+            //Context = context;
+            //Bids = new BidOnItemRepository(Context); //example
+        }
+        //interfaces for all repositories that are relevant for auctions
+        /*
+         * public IBidOnItemRepository Bids { get; private set; } //example
+         * CreateAuction
+         * EditAuction
+         * SearchForItem
+         */
+        public void Commit()
+        {
+            Context.SaveChanges();
+        }
+        public void Dispose()
+        {
+            Context.Dispose();
+        }
     }
 }
