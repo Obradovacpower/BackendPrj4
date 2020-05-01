ea71ce21b2c9864d793a4ea3145358cca1d937d4
Ermin Obradovac <au592117@uni.au.dk>
2020-05-01 17:31:32 +0200

    ICreateAuctionRepository done

---
+++ VareDatabase/VareDatabase/Interfaces/Auction/ICreateAuctionRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using VareDatabase.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace VareDatabase.Interfaces.Auction
+{
+    public interface ICreateAuctionRepository : IRepository<ItemEntity>
+    {
+        void AddItem(int buyOut, int userId, int expire, string[] tags, string title, string drescription,
+            string[] images);
+
+        void AddImage(int itemId, string image);
+
+        void GenerateTags(string nameOfItem, int itemId);
+
+        void AddTag(int itemId, string newTag);
+
+    }
+}
+
