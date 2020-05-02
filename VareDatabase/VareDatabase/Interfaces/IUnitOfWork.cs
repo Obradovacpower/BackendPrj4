70f62b8a17517930ba050975c14e7674d757149c
Ermin Obradovac <au592117@uni.au.dk>
2020-05-02 10:29:28 +0200

    bla bla bla

--- VareDatabase/VareDatabase/Interfaces/IUnitOfWork.cs
+++ VareDatabase/VareDatabase/Interfaces/IUnitOfWork.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using VareDatabase.Interfaces.Auction;
 
 namespace VareDatabase.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
         DbContext Context { get; }
+
+        IBidOnItemRepository Bids();
+        ICreateAuctionRepository Auctions();
+        IEditAuctionRepository Edits();
+        ISearchAuctionRepository Searches();
         void Commit();
         //IRepository GetRepository();
     }
